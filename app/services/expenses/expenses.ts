0731ea25a0dbd581295bb36b573f41a56184a1f6
Nil <avanilson.limasantos@gmail.com>
2026-09-01 20:18:06 -0300

    Added filter by user to requests

--- app/services/expenses/expenses.ts
+++ app/services/expenses/expenses.ts
@@ -31,24 +31,21 @@ export async function getExpenseByReference(reference: string) {
 }
 
 export async function createExpense(
-  // Omitimos o 'id' (gerado pelo banco) e o 'user_id' (injetado pelo backend)
   formData: Omit<Database['public']['Tables']['expenses']['Insert'], 'id' | 'user_id'>
 ) {
   const supabase = await createClient()
 
-  // 1. Pega o usuário logado direto dos cookies seguros do Supabase Auth
   const { data: { user }, error: authError } = await supabase.auth.getUser()
   if (authError || !user) throw new Error("Usuário não autenticado")
 
-  // 2. Insere os dados juntando o formulário com o ID do usuário verificado
   const { data, error } = await supabase
     .from('expenses')
     .insert({
       ...formData,
-      user_id: user.id // Injeção segura no servidor
+      user_id: user.id
     })
-    .select()        // Garante que o Supabase retorne os dados inseridos
-    .single()        // Avisa que é apenas um objeto (e não uma lista)
+    .select()
+    .single()
 
   if (error) throw new Error(error.message)
   return data
